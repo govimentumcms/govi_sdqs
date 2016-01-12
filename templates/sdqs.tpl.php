2151170b51a27e05794ea285ed46e50497f3a204
snaphuman <fhernandezn@alcaldiabogota.gov.co>
2016-01-12 17:56:19 -0500

    [sdqs] impementa formulario y visualización popup
    Los formularios que enviarán los datos a la API del SDQS estarán
    disponibles como recurso en urls específicas para crear y consultar PQRs
    
    Por otra parte los formularios serán enviados como páginas que contendrán
    las variables con los datos del web service
    
    Se requere colorbox como dependencia para gestionar la presentación
    en forma de poṕup

--- templates/sdqs.tpl.php
+++ templates/sdqs.tpl.php
@@ -1,6 +1,19 @@
-<?php
-$service = wsclient_service_load('sdqs');
-dsm($service);
-?>
+<div id="controles-sdqs">
 
-<h1>SDQS</h1>
+    <div class="boton crear">
+	<a href="/govi-sdqs/crear?width=500&height=500" class="colorbox colorbox-load" >Crear Petición</a>
+    </div>
+
+    <div class="boton consultar">
+	<a href="">Consultar Petición</a>
+    </div>
+
+    <div class="info">
+	<p>Si usted es un usuario registrado y desea crear o consultar un PQR, ingrese <a href="#">AQUI</a></p>
+	<p>Si usted quiere registrarse en el Sistema Distrital de Quejas y Soluciones, ingrese <a href="#">AQUI</a></p>
+    </div>
+    <div class="select servicios">
+	Más Servicios
+    </div>
+</div>
+
