1b200c3d8dc04fba51a503b0346ce9cc4ac21685
snaphuman <fhernandezn@alcaldiabogota.gov.co>
2016-01-26 11:51:09 -0500

    [sdqs] inicia estilos a los elementos del bloque

--- templates/sdqs.tpl.php
+++ templates/sdqs.tpl.php
@@ -1,20 +1,28 @@
-<?php //dsm(get_defined_vars()); ?>
+<?php //dsm(get_defined_vars()); 
+$css = drupal_get_path('module', 'govi_sdqs') . '/assets/css/govi-sdqs.css';
+drupal_add_css($css);
+?>
 
-<div id="controles-sdqs">
-
-    <div class="boton crear">
-	<a href="govi-sdqs/crear" class="colorbox-load colorbox-node" >Crear Petición</a>
-    </div>
-
-    <div class="boton consultar">
-	<a href="/govi-sdqs/consultar" class="colorbox-node">Consultar Petición</a>
-    </div>
-
-    <div class="info">
-	<p>Si usted es un usuario registrado y desea crear o consultar un PQR, ingrese <a href="#">AQUI</a></p>
-	<p>Si usted quiere registrarse en el Sistema Distrital de Quejas y Soluciones, ingrese <a href="#">AQUI</a></p>
-    </div>
-    <div class="select servicios">
-	Más Servicios
+<div id="servicio-ciudadano">
+    <div id="sdqs">
+	<div class="title"><h3><i class="logo"></i>Sistema Distrital de Quejas y Soluciones</h3></div>
+	<div class="controles">
+	    <div class="boton crear">
+		<i></i>
+		<a href="govi-sdqs/crear" class="colorbox-load colorbox-node" >Crear <span>Petición</span></a>
+	    </div>
+	    
+	    <div class="boton consultar">
+		<i></i>
+		<a href="/govi-sdqs/consultar" class="colorbox-node">Consultar <span>Petición</span></a>
+	    </div>
+	    
+	    <div class="info">
+		<p>Si usted quiere registrarse en el Sistema Distrital de Quejas y Soluciones, ingrese <a href="#">AQUI</a></p>
+	    </div>
+	    <div class="select servicios">
+		Más Servicios
+	    </div>
+	</div>
     </div>
 </div>
