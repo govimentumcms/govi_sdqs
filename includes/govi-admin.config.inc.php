aa9916096cdcacb51e5ebffb0e1877f77605afe8
ojardila <ojardila>
2018-05-28 16:50:30 -0500

    Adding config section to SDQS admin

--- includes/govi-admin.config.inc.php
+++ includes/govi-admin.config.inc.php
@@ -16,15 +16,22 @@ drupal_flush_all_caches();
 }
 
 
-function govi_sdqs_obtener_info_entidad($form, $form_state) {
+function govi_sdqs_entity_info($form, $form_state) {
   $sdqs =  SdqsClient::getInstance();
   $entity = $form_state['values']['govi_sdqs_entity'];
-  $entities_list = $sdqs->getDependencyList($entity);
   reset($entities_list);
+  $entities_list = $sdqs->getDependencyList($entity);
   $form['govi_sdqs_widget_settings']['govi_sdqs_dependency']['#options'] = $entities_list;
   return $form['govi_sdqs_widget_settings']['govi_sdqs_dependency'];
 }
-
+function govi_sdqs_theme_info($form, $form_state) {
+  $sdqs =  SdqsClient::getInstance();
+  $entity = $form_state['values']['govi_sdqs_entity'];
+  reset($theme_list);
+  $theme_list = $sdqs->getThemeList($entity);
+  $form['govi_sdqs_widget_settings']['govi_sdqs_theme']['#options'] = $theme_list;
+  return $form['govi_sdqs_widget_settings']['govi_sdqs_theme'];
+}
 
 /**
  * Form callback
@@ -58,61 +65,45 @@ function govi_sdqs_admin_settings() {
     '#description' => t('Entidad a la cuál van a ser enviadas las solicitudes'),
     '#options' => variable_get('sdqs_entities'),
     '#ajax' => array(
-            'event' => 'change',
-            'effect' => 'fade',
-            'callback' => 'govi_sdqs_obtener_info_entidad',
-            'method' => 'replace',
-            'wrapper' => 'wrapper-dependencia'
-        ),
+      'event' => 'change',
+      'effect' => 'fade',
+      'callback' => 'govi_sdqs_entity_info',
+      'method' => 'replace',
+      'wrapper' => 'wrapper-dependency'
+    ),
     '#default_value' => variable_get('govi_sdqs_entity', 0),
   );
   $form['govi_sdqs_widget_settings']['govi_sdqs_dependency'] = array(
     '#type' => 'select',
     '#title' => t('Dependencia'),
     '#description' => t('Entidad a la cuál van a ser enviadas las solicitudes'),
-    '#prefix' => '<div id="wrapper-dependencia">',
+    '#prefix' => '<div id="wrapper-dependency">',
     '#suffix' => '</div>',
-    '#options' => array(),
+    '#options' => $sdqs->getDependencyList(variable_get('govi_sdqs_entity')),
+    '#ajax' => array(
+      'event' => 'change',
+      'effect' => 'fade',
+      'callback' => 'govi_sdqs_theme_info',
+      'method' => 'replace',
+      'wrapper' => 'wrapper-sdqs-theme'
+    ),
     '#validated' => TRUE,
     '#default_value' => variable_get('govi_sdqs_dependency', 0),
   );
-  $form['govi_sdqs_widget_settings']['govi_sdqs_size'] = array(
-    '#default_value' => variable_get('govi_sdqs_size', ''),
-    '#description' => t('The size of CAPTCHA to serve.'),
-    '#options' => array(
-      '' => t('Normal (default)'),
-      'compact' => t('Compact'),
-    ),
-    '#title' => t('Size'),
+
+  $form['govi_sdqs_widget_settings']['govi_sdqs_theme'] = array(
     '#type' => 'select',
-  );
-  $form['govi_sdqs_widget_settings']['govi_sdqs_tabindex'] = array(
-    '#type' => 'textfield',
-    '#title' => t('Tabindex'),
-    '#description' => t('Set the <a href="@tabindex">tabindex</a> of the widget and challenge (Default = 0). If other elements in your page use tabindex, it should be set to make user navigation easier.', array('@tabindex' => 'http://www.w3.org/TR/html4/interact/forms.html#adef-tabindex')),
-    '#default_value' => variable_get('govi_sdqs_tabindex', 0),
-    '#size' => 4,
-  );
-  $form['govi_sdqs_widget_settings']['govi_sdqs_noscript'] = array(
-    '#type' => 'checkbox',
-    '#title' => t('Enable fallback for browsers with JavaScript disabled'),
-    '#default_value' => variable_get('govi_sdqs_noscript', 0),
-    '#description' => t('If JavaScript is a requirement for your site, you should <strong>not</strong> enable this feature. With this enabled, a compatibility layer will be added to the captcha to support non-js users.'),
+    '#title' => t('Tema'),
+    '#description' => t('Tema principal a través del cuál van a ser enviadas las solicitudes'),
+    '#prefix' => '<div id="wrapper-sdqs-theme">',
+    '#suffix' => '</div>',
+    '#options' => $sdqs->getThemeList(variable_get('govi_sdqs_entity')),
+    '#validated' => TRUE,
+    '#default_value' => variable_get('govi_sdqs_theme', 0),
   );
 
   return system_settings_form($form);
 }
 function govi_sdqs_admin_update_data() {
   drupal_goto('admin/config/service-sdqs-update');
-}
-/**
- * Validation function for govi_sdqs_admin_settings().
- *
- * @see govi_sdqs_admin_settings()
- */
-function govi_sdqs_admin_settings_validate($form, &$form_state) {
-  $tabindex = $form_state['values']['govi_sdqs_tabindex'];
-  if (!is_numeric($tabindex)) {
-    form_set_error('govi_sdqs_tabindex', t('The tabindex must be an integer.'));
-  }
 }
