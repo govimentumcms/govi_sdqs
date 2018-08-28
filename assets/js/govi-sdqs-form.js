208b0a4eaea940b8422f38c412521cfd0f2773c8
ojardila <ojardila>
2018-08-28 18:14:17 -0500

    Adding behavior custom identification types when 'Personas Jurídicas' and 'establecimientos comerciales' are selected

--- assets/js/govi-sdqs-form.js
+++ assets/js/govi-sdqs-form.js
@@ -1,5 +1,4 @@
 (function($) {
-
   function cambiarTipoID(t) {
     if (t.val() == '2') {
       jQuery(t).closest('fieldset').find('.form-item-razon-social').css('display','block');
@@ -25,12 +24,36 @@
         jQuery('.contact legend').text("Ubicación y contacto");
         jQuery('.contact .email span.form-required').css('display','block');
         jQuery('.identificacion legend').text("Datos de identificación");
+        jQuery('[name="tipo_identificacion"]').html('');
+        window.sqsd_tipos_id.forEach(function(opt){
+          jQuery('[name="tipo_identificacion"]').append('<option value="'+opt.id+'">'+opt.name+'</option>');
+        });
 
+    }
+    function actualizarTipoIdentificacion(identificaciones_validas) {
+      actual=window.sqsd_tipos_id.map(function (el) { return el.id; });
+      opciones_eliminar_identificaciones = actual.filter(x => !identificaciones_validas.includes(x));
+      opciones_eliminar_identificaciones.forEach(function(x){
+        jQuery("[name='tipo_identificacion'] option[value='"+x+"']").remove();
+      });
     }
 
     function cambiarTipoIdentificacion(t) {
         if (t.val() == 'juridica') {
             restaurarFormulario();
+            actualizarTipoIdentificacion(Array('1','2'));
+            jQuery('[name="tipo_identificacion"]').val('2');
+            jQuery('[name="tipo_identificacion"]').val('2');
+
+            jQuery('.form-item-genero').parent().css('display','none');
+            jQuery('.form-item-razon-social').css('display','block');
+            jQuery('.apoderado').css('display','none');
+
+        }
+        else if (t.val() == 'juridica_comercial') {
+            restaurarFormulario();
+            actualizarTipoIdentificacion(Array('2'));
+            jQuery('[name="tipo_identificacion"]').val('2');
             jQuery('[name="tipo_identificacion"]').val('2');
             jQuery('.form-item-genero').parent().css('display','none');
             jQuery('.form-item-razon-social').css('display','block');
@@ -83,6 +106,7 @@
     Drupal.behaviors.sdqsForm = {
         attach: function(context, settings) {
             jQuery(document).ready(function() {
+                window.sqsd_tipos_id = jQuery('[name="tipo_identificacion"] option').map(function() { return {id:jQuery(this).val(),name:jQuery(this).text()}; }).get();
                 var selector_tipo_persona = jQuery('[name="tipo_peticionario"]');
                 cambiarTipoIdentificacion(selector_tipo_persona);
                 selector_tipo_persona.change(function() {
